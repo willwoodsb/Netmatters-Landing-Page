2a05168fd92929e5c0dd1f3d4b40894c3f3bbf4b
Will Woods Ballard <willwoodsb@gmail.com>
2023-02-28 10:23:04 +0000

    altered connection file

--- inc/connection.php
+++ inc/connection.php
@@ -1,10 +1,13 @@
 <?php
 
+$password = "j3J8mbYeeF2FkqR";
+$username = "wwoodsba_wwoodsballard";
+$database = "wwoodsba_netmatters";
 $servername = "localhost";
-$database = "netmatters";
+
 
 try {
-    $db = new PDO("mysql:host=$servername;dbname=$database", 'root', '');
+    $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 } catch (Exception $e) {
     echo "error connecting to database";
     echo $e->getMessage();
